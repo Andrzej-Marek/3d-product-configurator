5ad39be6b842deca8f8026aa95ed4702689d95fe
Andrzej Marek <andrzejmarek@Andrzejs-Mac-Studio.local>
2023-08-25 10:21:35 +0200

    favi

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
+// import { ViteFaviconsPlugin } from "vite-plugin-favicon";
+import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteFaviconsPlugin("./public/muscle_logo.svg")],
+  plugins: [react(), vitePluginFaviconsInject("./public/muscle_logo.svg")],
 });
